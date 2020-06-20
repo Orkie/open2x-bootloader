6a9d7a9c9445dc6bb136d8b870e34fbdd447c2cc
Adan Scotney <adan.scotney@gmail.com>
2020-06-20 12:01:00 +0100

    Button repeat now working

--- source/main.c
+++ source/main.c
@@ -28,6 +28,134 @@ void loadSettings() {
   autoloadKernel = false;
 }
 
+struct ButtonHeldTime {
+  int A_held;
+  int B_held;
+  int X_held;
+  int Y_held;
+  int START_held;
+  int SELECT_held;
+  int UP_held;
+  int DOWN_held;
+  int LEFT_held;
+  int RIGHT_held;
+  int UP_LEFT_held;
+  int UP_RIGHT_held;
+  int DOWN_LEFT_held;
+  int DOWN_RIGHT_held;
+  int VOL_UP_held;
+  int VOL_DOWN_held;
+  int STICK_held;
+  int L_held;
+  int R_held;
+};
+
+#define REPEAT_INITIAL_MS 400
+#define REPEAT_MS 50
+
+uint32_t decideNewPresses(uint32_t current, uint32_t last, struct ButtonHeldTime* heldTime, int msSinceLastLoop) {
+
+  heldTime->A_held = (current & A) ? heldTime->A_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->B_held = (current & B) ? heldTime->B_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->X_held = (current & X) ? heldTime->X_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->Y_held = (current & Y) ? heldTime->Y_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->START_held = (current & START) ? heldTime->START_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->SELECT_held = (current & SELECT) ? heldTime->SELECT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->UP_held = (current & UP) ? heldTime->UP_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->DOWN_held = (current & DOWN) ? heldTime->DOWN_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->LEFT_held = (current & LEFT) ? heldTime->LEFT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->RIGHT_held = (current & RIGHT) ? heldTime->RIGHT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->UP_LEFT_held = (current & UP_LEFT) ? heldTime->UP_LEFT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->UP_RIGHT_held = (current & UP) ? heldTime->UP_RIGHT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->DOWN_LEFT_held = (current & DOWN_LEFT) ? heldTime->DOWN_LEFT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->DOWN_RIGHT_held = (current & DOWN_RIGHT) ? heldTime->DOWN_RIGHT_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->VOL_UP_held = (current & VOL_UP) ? heldTime->VOL_UP_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->VOL_DOWN_held = (current & VOL_DOWN) ? heldTime->VOL_DOWN_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->STICK_held = (current & STICK) ? heldTime->STICK_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->L_held = (current & L) ? heldTime->L_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  heldTime->R_held = (current & R) ? heldTime->R_held - msSinceLastLoop : REPEAT_INITIAL_MS;
+  
+  uint32_t repeatPresses = 0;
+  if(heldTime->A_held <= 0) {
+    heldTime->A_held += REPEAT_MS;
+    repeatPresses |= A;
+  }
+  if(heldTime->B_held <= 0) {
+    heldTime->B_held += REPEAT_MS;
+    repeatPresses |= B;
+  }
+  if(heldTime->X_held <= 0) {
+    heldTime->X_held += REPEAT_MS;
+    repeatPresses |= X;
+  }
+  if(heldTime->Y_held <= 0) {
+    heldTime->Y_held += REPEAT_MS;
+    repeatPresses |= Y;
+  }
+  if(heldTime->START_held <= 0) {
+    heldTime->START_held += REPEAT_MS;
+    repeatPresses |= START;
+  }
+  if(heldTime->SELECT_held <= 0) {
+    heldTime->SELECT_held += REPEAT_MS;
+    repeatPresses |= SELECT;
+  }
+  if(heldTime->UP_held <= 0) {
+    heldTime->UP_held += REPEAT_MS;
+    repeatPresses |= UP;
+  }
+  if(heldTime->DOWN_held <= 0) {
+    heldTime->DOWN_held += REPEAT_MS;
+    repeatPresses |= DOWN;
+  }
+  if(heldTime->LEFT_held <= 0) {
+    heldTime->LEFT_held += REPEAT_MS;
+    repeatPresses |= LEFT;
+  }
+  if(heldTime->RIGHT_held <= 0) {
+    heldTime->RIGHT_held += REPEAT_MS;
+    repeatPresses |= RIGHT;
+  }
+  if(heldTime->UP_LEFT_held <= 0) {
+    heldTime->UP_LEFT_held += REPEAT_MS;
+    repeatPresses |= UP_LEFT;
+  }
+  if(heldTime->UP_RIGHT_held <= 0) {
+    heldTime->UP_RIGHT_held += REPEAT_MS;
+    repeatPresses |= UP_RIGHT;
+  }
+  if(heldTime->DOWN_LEFT_held <= 0) {
+    heldTime->DOWN_LEFT_held += REPEAT_MS;
+    repeatPresses |= DOWN_LEFT;
+  }
+  if(heldTime->DOWN_RIGHT_held <= 0) {
+    heldTime->DOWN_RIGHT_held += REPEAT_MS;
+    repeatPresses |= DOWN_RIGHT;
+  }
+  if(heldTime->VOL_UP_held <= 0) {
+    heldTime->VOL_UP_held += REPEAT_MS;
+    repeatPresses |= VOL_UP;
+  }
+  if(heldTime->VOL_DOWN_held <= 0) {
+    heldTime->VOL_DOWN_held += REPEAT_MS;
+    repeatPresses |= VOL_DOWN;
+  }
+  if(heldTime->STICK_held <= 0) {
+    heldTime->STICK_held += REPEAT_MS;
+    repeatPresses |= STICK;
+  }
+  if(heldTime->L_held <= 0) {
+    heldTime->L_held += REPEAT_MS;
+    repeatPresses |= L;
+  }
+  if(heldTime->R_held <= 0) {
+    heldTime->R_held += REPEAT_MS;
+    repeatPresses |= R;
+  }
+  
+  return (current&(~last)) | repeatPresses;
+}
+
 int main() {
   gp2xInit();
   setbuf(stdout, NULL);
@@ -66,7 +194,11 @@ int main() {
   int gotChar = EOF;
 
   uint32_t lastTick = timerGet();
-
+  struct ButtonHeldTime heldTime = { REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS,
+				     REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS,
+				     REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS, REPEAT_INITIAL_MS,
+				     REPEAT_INITIAL_MS};
+  
   while(1) {
 
     // terminal character handler
@@ -82,7 +214,7 @@ int main() {
 
     // deal with current state
     if(currentView != NULL) {
-      currentView->handleInput(currentButtonState, currentButtonState&(~previousButtonState));
+      currentView->handleInput(currentButtonState, decideNewPresses(currentButtonState, previousButtonState, &heldTime, msSinceLastLoop));
     }
     previousButtonState = currentButtonState;
 
